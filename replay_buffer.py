b9c7a2c75cb3961db23641299657d6cefc67c246
Sang baek, Park <108871750+CAI23sbP@users.noreply.github.com>
2023-11-02 02:06:34 +0900

    Update replay_buffer.py
    add tensorboard for history of return and q_value loss

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,7 +1,7 @@
 
 import random,torch, os
 from collections import namedtuple, deque
-
+from torch.utils.tensorboard import SummaryWriter
 
 class ReplayBuffer():
     def __init__(self, configs, predict_net, target_net, opt_lossf):
@@ -14,6 +14,8 @@ class ReplayBuffer():
         self.predict_net = predict_net
         self.target_net = target_net 
         self.optimizer, self.criterion = opt_lossf
+        self.writer = SummaryWriter(os.getcwd())
+        self.iter = 0
 
     def add(self, *args):
         self.memory.append(self.Transition(*args))
@@ -26,7 +28,7 @@ class ReplayBuffer():
 
     def init_batch(self):
         self.state_batch = torch.zeros(self.config.Buffer.BATCH_SIZE,self.config.Env.observation_space)
-        self.action_batch = torch.zeros(self.config.Buffer.BATCH_SIZE,self.config.Env.action_space.n,dtype=torch.int64)
+        self.action_batch = torch.zeros(self.config.Buffer.BATCH_SIZE,self.config.Env.action_space.n, dtype=torch.int64)
         self.next_state_batch = torch.zeros(self.config.Buffer.BATCH_SIZE,self.config.Env.observation_space)
         self.reward_batch = torch.zeros(self.config.Buffer.BATCH_SIZE,1)
         self.done_batch = torch.zeros(self.config.Buffer.BATCH_SIZE,1,dtype = torch.bool)
@@ -37,7 +39,7 @@ class ReplayBuffer():
         if len(self.memory) < self.config.Buffer.BATCH_SIZE:
             return
         dataset = self.sample()
-        
+        self.iter +=1
         for i, data in enumerate([*dataset]):
             self.state_batch[i] = data[0]
             self.action_batch [i] = data[1] 
@@ -45,17 +47,29 @@ class ReplayBuffer():
             self.reward_batch [i] = data[3]
             self.done_batch [i] = data[4] 
 
-        predict_q_value = self.predict_net(self.state_batch).gather(1,self.action_batch)
+        predict_q_value = self.predict_net(self.state_batch).gather(1, self.action_batch)
 
         with torch.no_grad():
             next_states = torch.cat([next_state for done, next_state in zip(self.done_batch, self.next_state_batch) if not done]).reshape(-1,4)
             self.next_value[~self.done_batch.view(-1)] = self.target_net(next_states).max(1)[0]
-        
+            rewards = self.reward_batch[~self.done_batch.view(-1)]
+            G_return = 0
+            for reward in rewards[-2]:
+                G_return += reward.item()*self.config.Network.GAMMA 
+            G_return+=rewards[-1].item()
         expected_func = predict_q_value * self.config.Network.GAMMA + self.reward_batch
+
         loss = self.criterion(predict_q_value, expected_func)
+
+        tag_scalar_loss= {"Q_value_loss": loss}
+        tag_scalar_return= {"return": G_return}
+        self.writer.add_scalars("return",tag_scalar_return, self.iter)
+        self.writer.add_scalars("Q_value_loss",tag_scalar_loss, self.iter)
+        
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+        
         target_net_state_dict = self.target_net.state_dict()
         policy_net_state_dict = self.predict_net.state_dict()
         for key in policy_net_state_dict:
@@ -72,4 +86,3 @@ class ReplayBuffer():
         print(f"load path {path}")
         self.target_net.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage))
         self.predict_net.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage))
-
